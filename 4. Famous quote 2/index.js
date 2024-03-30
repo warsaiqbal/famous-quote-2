bb04c579edd5111fd27f93db52f9fd7eb76f911d
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-03-30 06:16:29 -0700

    Add files via upload

---
+++ 4. Famous quote 2/index.js
@@ -0,0 +1,3 @@
+var famous_person = "Quaid e azam";
+var message = " once said, 'I don't believe in taking right decisions. I take decisions and make them right.'";
+console.log(famous_person + message);
---
+++ 4. Famous quote 2/index.ts
@@ -0,0 +1,3 @@
+let famous_person: string = "Quaid e azam"
+let message: string = " once said, 'I don't believe in taking right decisions. I take decisions and make them right.'"
+console.log(famous_person + message);
